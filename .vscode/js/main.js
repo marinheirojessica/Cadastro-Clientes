88061efcd764346f8c5f4ba9c62b74ee6ea6ecc2
marinheirojessi <marinheirojessi20@gmail.com>
2023-10-03 11:12:40 -0300

    Fiz algumas alterações no código

--- .vscode/js/main.js
+++ .vscode/js/main.js
@@ -1,13 +1,21 @@
-let clientes =[];
+let clientes = [];
+  
+function salvarCliente() {
+    console.log('Função salvarCliente foi chamada.');
 
-function salvarClientes() {
     const nome =  document.getElementById('nome').value;
     const email = document.getElementById('email').value;
     const telefone = document.getElementById('telefone').value;
     const cpf = document.getElementById('cpf').value;
 
+    console.log('Nome:', nome);
+    console.log('Email:', email);
+    console.log('Telefone:', telefone);
+    console.log('CPF:', cpf);
+
     const cliente = { nome, email, telefone, cpf};
     clientes.push(cliente);
+    console.log('Cliente salvo:, cliente');
 
     //Limpar os campos após salvar
     document.getElementById('nome').value = '';
@@ -19,11 +27,17 @@ function salvarClientes() {
 }
 
 function listarClientes() {
+    console.log('Função listarClientes foi chamada');
+
     const listaClientes = document.getElementById('listaClientes');
     listaClientes.innerHTML = '';
 
+    console.log('Clientes:', clientes);
+
+
     if(clientes.length === 0) {
-        listaClientes.innerHTML = <li>Nenhum cliente cadastrado.</li>;
+        console.log('Nenhum cliente cadastrado.')
+        listaClientes.innerHTML = '<li>Nenhum cliente cadastrado.</li>';
         return;
     }
 
