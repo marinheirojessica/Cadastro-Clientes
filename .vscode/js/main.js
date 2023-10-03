95d3a4e610665f3215f0bc002585f1c8fd61185b
marinheirojessi <marinheirojessi20@gmail.com>
2023-10-02 23:38:48 -0300

    Iniciei o formulário criando em html e css e exibindo.

---
+++ .vscode/js/main.js
@@ -0,0 +1,8 @@
+let clientes =[];
+
+function salvarClientes() {
+    const nome =  document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const telefone = document.getElementById('telefone').nodeValue
+    const cpf =
+}
